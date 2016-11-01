eb64419e3b0877fff67d44f2b3ec6d20558e7a6e
Alatyami <jehunt@ymail.com>
2016-11-01 07:45:45 -0400

    Minor Update

--- src/main/java/growthcraft/core/GrowthCraftCore.java
+++ src/main/java/growthcraft/core/GrowthCraftCore.java
@@ -1,10 +1,11 @@
 package growthcraft.core;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-/**
- * Created by Alatyami on 10/15/2016.
- */
 @Mod(
         modid = GrowthCraftCore.MOD_ID,
         name = GrowthCraftCore.MOD_NAME,
@@ -19,7 +20,19 @@ public class GrowthCraftCore {
     public static final String MOD_ACC_MINECRAFT = "[1.10.2]";
     public static final String MOD_DEPENDENCIES = "required-after:Forge@[10.13.4.1566,)";
 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
 
+    }
 
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
 
 }
